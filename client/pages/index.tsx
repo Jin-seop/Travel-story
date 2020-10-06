da3d0e4a1971431638cd3fee2a0d04ba8541bc4a
JinSeop <turn3361@gmail.com>
2020-10-06 21:35:04 +0900

    추가 : client
     - 타이틀 설정 완료
     - 파비콘은 서버에서 할것!

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,14 @@
 import Header from "~/components/header";
 import classes from "./style.module.scss";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
     <div className={classes.HomePage}>
+      <Head>
+        <title>Travel Story</title>
+        <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
+      </Head>
       <Header> Next App </Header>
       Welcome!
     </div>
