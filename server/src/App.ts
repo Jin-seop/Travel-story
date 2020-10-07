884dd6bd8d71d5af1f767abebe0a6d037b68716a
JinSeop <turn3361@gmail.com>
2020-10-07 11:29:07 +0900

    미완성 : favicon
     - favicon 패키지 설치
     - App에서 배포시에 설정할 것!

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import * as favicon from 'express-favicon';
 
 class App {
     public app: express.Application;
@@ -17,6 +18,10 @@ class App {
     constructor() {
         this.app = express();
         this.app.use(cors());
+
+        //배포시 경로 바꿔서 파비콘 적용하기(정적 파일제공 참고)
+        // this.app.use(favicon(__dirname + '../images/favicon.ico'));
+
         this.app.get(
             '/',
             (
