6b6767abf15bd2d206ab6a04355dfd6af80039ec
JinSeop <turn3361@gmail.com>
2020-10-07 10:59:51 +0900

    추가 : 패키지
     - cors설치

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,31 +1,31 @@
 import * as express from 'express';
 
 class App {
-  public app: express.Application;
+    public app: express.Application;
 
-  /**
-   * @ class App
-   * @ method bootstrap
-   * @ static
-   *
-   */
-  public static bootstrap(): App {
-    return new App();
-  }
+    /**
+     * @ class App
+     * @ method bootstrap
+     * @ static
+     *
+     */
+    public static bootstrap(): App {
+        return new App();
+    }
 
-  constructor() {
-    this.app = express();
-    this.app.get(
-      '/',
-      (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        res.send('Hello world');
-      }
-    );
-  }
+    constructor() {
+        this.app = express();
+        this.app.get(
+            '/',
+            (
+                req: express.Request,
+                res: express.Response,
+                next: express.NextFunction
+            ) => {
+                res.send('Hello world');
+            }
+        );
+    }
 }
 
 export default App;
