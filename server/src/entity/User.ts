f72d175d33e6b98459851598e7e956d033e16ccf
김도연 <doyeon@gimdoyeon-ui-MacBookPro.local>
2020-10-08 10:46:36 +0900

    추가:테이블생성 및 관계설정

---
+++ server/src/entity/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Content } from './Content';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @OneToMany(() => Content, (content) => content.user)
+    contents: Content[];
+}
