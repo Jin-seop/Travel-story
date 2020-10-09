d183ae1a41eff932abd4039d715c30de318d4dc8
JinSeop <turn3361@gmail.com>
2020-10-09 10:21:52 +0900

    추가 : typeorm 검색예시
     - User검색 예시

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,38 +1,65 @@
 import 'reflect-metadata';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
 import { Content } from './entity/Content';
 
 createConnection()
     .then(async (connection) => {
-        // console.log('Inserting a new user into the database...');
-        // const user = new User();
-        // user.email = 'admin@gmail.com';
-        // user.username = 'admin';
-        // user.password = 'passwordtest';
-        // await connection.manager.save(user);
-        // console.log('Saved a new user with id: ' + user.id);
+        class App {
+            public app: express.Application;
 
-        // console.log('Loading users from the database...');
-        // const users = await connection.manager.find(User);
-        // console.log('Loaded users: ', users);
+            /**
+             * @ class App
+             * @ method bootstrap
+             * @ static
+             *
+             */
+            public static bootstrap(): App {
+                return new App();
+            }
 
-        // console.log(
-        //     'Here you can setup and run express/koa/any other framework.'
-        // );
-        const content1 = new Content();
-        content1.title = 'me.jpg';
-        await connection.manager.save(content1);
+            constructor() {
+                this.app = express();
+                this.app.use(
+                    cors({
+                        origin: '*',
+                        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+                        credentials: true,
+                    })
+                );
+                this.app.use(bodyParser.json());
+                this.app.use(
+                    bodyParser.urlencoded({
+                        extended: false,
+                    })
+                );
 
-        const content2 = new Content();
-        content2.title = 'me-and-bears.jpg';
-        await connection.manager.save(content2);
+                //배포시 경로 바꿔서 파비콘 적용하기(정적 파일제공 참고)
+                // this.app.use(favicon(__dirname + '../images/favicon.ico'));
 
-        const user = new User();
-        user.username = 'John Cena';
-        user.email = 'wwe@wwe.com';
-        user.password = 'passwordtest';
-        user.contents = [content1, content2];
-        await connection.manager.save(user);
+                this.app.get(
+                    '/',
+                    async (
+                        req: express.Request,
+                        res: express.Response,
+                        next: express.NextFunction
+                    ) => {
+                        const userRepository = connection.getRepository(User);
+                        const users = await userRepository.find();
+                        res.json(users);
+                    }
+                );
+            }
+        }
+
+        const port: number = Number(process.env.PORT) || 4000;
+        const app: express.Application = new App().app;
+
+        app.listen(port, () =>
+            console.log(`Express server listening at ${port}`)
+        ).on('error', (err) => console.error(err));
     })
     .catch((error) => console.log(error));
