7e7a5c7f17fa80a2bd21432c2356f60ae51e6ea7
rladbgus <cocokiuuu1858@gmail.com>
2020-10-19 13:27:51 +0900

    수정: 정보수정 기능 구현 완료

--- client/components/LoginPage/MypageBody.tsx
+++ client/components/LoginPage/MypageBody.tsx
@@ -6,39 +6,34 @@ import {useSelector} from "react-redux";
 
 const MypageBody = () => {
   const route = useRouter();
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [token, setToken] = useState("")
+  const [email, setEmail] = useState("");
   const username = useSelector((state) => state.userName);
 
   const handleMyInfo = (e) => {
     e.preventDefault();
     Axios.put("http://localhost:4000/editUser", {
-      token: token,
-      password: password,
-      email: email
+      email: email,
+      password: password
     })
-    .then((res) => {
-      console.log(res);
-      alert('수정이 완료되었습니다')
-      route.push('/')
-    })
-    .catch((err) => {console.log(err)})
+      .then(() => {
+        alert('수정이 완료되었습니다')
+        route.push('/')
+      })
+      .catch((err) => { console.log(err) })
   }
 
-  useEffect(() => {
-    setToken(localStorage.getItem('token'))
-  }, [])
-
-  return(
+  return (
     <div className={style.LoginBodyContainer}>
     <div className={style.LoginContainer}>
-      <p>{`${username}님 수정할 정보를 입력해 주세요 :)`}</p>
+      <p>{`${username}님 정보를 입력해 주세요 :)`}</p>
       <form>
+        <div>현재 이메일 주소를 입력해 주세요</div>
         <input placeholder="Email" type="email" onChange={(e) => {
           e.preventDefault()
           setEmail(e.target.value)
         }} />
+        <div>새로운 비밀번호를 입력해 주세요</div>
         <input placeholder="Password" type="password" onChange={(e) => {
           e.preventDefault()
           setPassword(e.target.value)
